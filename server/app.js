7ef2ba2b05fb266ddc5e4ad71832cb57593119c2
D B Prem Bhuvan <dbprembhuvan@Ds-MacBook-Air.local>
2026-03-09 22:31:28 +0530

    final

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.set('trust proxy', 1); // Trust first proxy (Render)
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 app.use(express.json());
